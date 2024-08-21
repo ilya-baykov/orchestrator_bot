9912faa2619405462fab888eda566047fc829dc0
ilyab <i-baykov@internet.ru>
2024-08-21 14:58:07 +0300

    Создал основу для будущих хендлеров

---
+++ BOT/app/bot_running.py
@@ -0,0 +1,19 @@
+from os import environ
+
+from aiogram import Dispatcher, Bot
+
+from app.handlers.start.registration import register_start_handlers
+
+dp = Dispatcher()
+bot = Bot(token=environ.get('TOKEN', 'define me!'))
+
+
+async def start_bot():
+    # Регистрация обработчиков
+
+    register_start_handlers(dp)  # Стартовый обработчик
+    # register_edit_handlers(dp)  # Обработчик для редактирования отчетов
+    # register_user_response(dp)  # Обработчик ответов сотрудников
+    # register_unknown_command(dp)  # Обработчик неизвестных команд
+
+    await dp.start_polling(bot, skip_updates=True)
--- BOT/main.py
+++ BOT/main.py
@@ -1,16 +1,27 @@
 import asyncio
+import platform
+from aiogram import Bot
+from app.bot_running import start_bot
+from database.TelegramUser.crud import TelegramUserCRUD
 from database.core import db
-from database.CRUD.create import create_user
+from logger_settings import setup_logger
 
 
 async def main():
-    # Создаем базу данных и таблицы
-    await db.create_db()
+    # await db.reset_database()
+    # await db.create_db()
 
-    # Теперь можно создать пользователя
-    user = await create_user(username="ilya", email="ilya@baykov", full_name="Байков Илья Павлович")
-    print(f"Создан пользователь: {user}")
+    await start_bot()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    print("Бот запущен")
+    logger = setup_logger()
+    try:
+        # Установите политику цикла событий для Windows
+        if platform.system() == 'Windows':
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+        asyncio.run(main())
+    except Exception as e:
+        print(e)
