989574b0d34ac07c43a6e5cfdce086c7afac514d
ilyab <i-baykov@internet.ru>
2024-08-22 14:36:46 +0300

    Добавли в пуллинг обработку запросов на получение информации по процессу

--- BOT/app/bot_running.py
+++ BOT/app/bot_running.py
@@ -2,6 +2,7 @@ from os import environ
 
 from aiogram import Dispatcher, Bot
 
+from app.handlers.get_processes.select_process import register_orchestrator_process_handlers
 from app.handlers.start.registration import register_start_handlers
 
 dp = Dispatcher()
@@ -12,6 +13,7 @@ async def start_bot():
     # Регистрация обработчиков
 
     register_start_handlers(dp)  # Стартовый обработчик
+    register_orchestrator_process_handlers(dp)
     # register_edit_handlers(dp)  # Обработчик для редактирования отчетов
     # register_user_response(dp)  # Обработчик ответов сотрудников
     # register_unknown_command(dp)  # Обработчик неизвестных команд
