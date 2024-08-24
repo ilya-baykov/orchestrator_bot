e17de375ee2926e1377cfd69a2c04a2b7f9708c1
ilyab <i-baykov@internet.ru>
2024-08-24 13:42:29 +0300

    Пересобрал логику получения процессов

--- BOT/app/handlers/get_processes/select_process.py
+++ BOT/app/handlers/get_processes/select_process.py
@@ -1,4 +1,5 @@
 import logging
+from difflib import get_close_matches
 
 from aiogram import Router, F
 from aiogram.filters import Command
@@ -7,6 +8,9 @@ from aiogram.types import Message
 
 from app.handlers.get_processes.state import OrchestratorProcessState
 from app.handlers.get_processes.utility import ProcessSearcher
+from database.TelegramUser.crud import TelegramUserCRUD
+from database.UserInput.crud import UserInputCRUD
+# from app.handlers.get_processes.utility import ProcessSearcher
 from global_filter import RegisteredUser
 
 logger = logging.getLogger(__name__)
@@ -22,22 +26,20 @@ async def select_processor(message: Message, state: FSMContext):
 
 @orchestrator_process.message(OrchestratorProcessState.input_process_prefix)
 async def search_process(message: Message, state: FSMContext):
+    """Обрабатывает сообщение и ищет процессы по префиксу."""
+
     try:
+        processes = await ProcessSearcher.find_processes(str(message.from_user.id))  # Объект для поиска процессов
+        suitable_processes = ProcessSearcher.get_suitable_processes(processes, message.text)  # Подходящие процессы
 
-        process_searcher = await ProcessSearcher.create(input_process=message.text,
-                                                        telegram_id=str(message.from_user.id))
-        processes_names = process_searcher.get_processes_by_prefix()  # Получаем имена процессов по префиксу
-
-        if processes_names:  # Если такой префикс найден среди процессов - возвращаем имена процессов с таким же префиксом
-            await message.answer(str(processes_names))
-            await state.set_state(OrchestratorProcessState.input_current_process)
+        if suitable_processes:
+            await message.answer(str(suitable_processes))
         else:
-            matches = process_searcher.get_similar()  # Поиск похожих процессов
+            matches = ProcessSearcher.get_close_matches(processes, message.text)  # Похожие по префиксу названия
             if matches:
-                await message.answer(f"Возможно, вы имели в виду: {str(matches)}")
+                await message.answer(f"Возможно вы имели в виду: {str(matches)}")
             else:
-                await message.answer("Такие процессы не найдены.")
-
+                await message.answer("Такой процесс не найден")
     except Exception as e:
         await message.answer("Произошла ошибка при поиске процессов.")
         logger.error(f"При попытке найти нужные процессы по префиксу произошла ошибка: {e}")
--- BOT/app/handlers/get_processes/utility.py
+++ BOT/app/handlers/get_processes/utility.py
@@ -1,33 +1,43 @@
 from difflib import get_close_matches
 from typing import List
 
-from app.support_objects import UserInputDb
 from database.TelegramUser.crud import TelegramUserCRUD
+from database.UserInput.crud import UserInputCRUD
 
 
 class ProcessSearcher:
-    processes_objects = None
-    all_process_names = None
 
-    def __init__(self, input_process: str):
-        self.input_process = input_process
-        self.input_prefix = input_process.upper().split("_")[0]
+    @staticmethod
+    async def find_processes(telegram_id: str) -> List:
+        """
+        Находит процессы для заданного Telegram ID.
 
-    @classmethod
-    async def create(cls, input_process: str, telegram_id: str):
-        instance = cls(input_process)
-        department_access = await TelegramUserCRUD.get_department_by_telegram_id(telegram_id)
-        instance.processes_objects = await UserInputDb.get_processes_objects(department_access=department_access)
-        instance.all_process_names = await UserInputDb.get_all_process_names()
-        return instance
+        :param telegram_id: ID пользователя в Telegram.
+        :return: Список процессов, доступных для данного пользователя.
+        """
+        department = await TelegramUserCRUD.get_department_by_telegram_id(telegram_id)
+        processes = await UserInputCRUD.find_all(department_access=department)
+        return processes
 
-    def get_processes_by_prefix(self):
-        return [process.name.upper() for process in self.processes_objects if
-                self.input_prefix == process.name.split("_")[0]]
+    @staticmethod
+    def get_suitable_processes(processes: List, search_text: str) -> List:
+        """
+        Находит подходящие процессы по заданному тексту.
 
-    def get_similar(self):
-        all_names = {prefix_name.split("_")[0] for prefix_name in self.all_process_names}  # Все имена процессов
-        matches = get_close_matches(self.input_prefix, all_names, n=10, cutoff=0.8)  # Поиск наиболее похожих
-        if matches:
-            return [match.upper() for match in matches]
-        return None
+        :param processes: Список процессов.
+        :param search_text: Текст для поиска.
+        :return: Список подходящих процессов.
+        """
+        return [process for process in processes if process.process_name.upper() == search_text.upper()]
+
+    @staticmethod
+    def get_close_matches(processes: List, search_text: str) -> List[str]:
+        """
+        Находит близкие совпадения для заданного текста.
+
+        :param processes: Список процессов.
+        :param search_text: Текст для поиска.
+        :return: Список близких совпадений.
+        """
+        all_names = {process.process_name.upper() for process in processes}
+        return get_close_matches(search_text.upper(), all_names, n=10, cutoff=0.7)
