a3afd3699f570a7456c202e1c05ebcdb570ca7b1
ilyab <i-baykov@internet.ru>
2024-09-12 16:20:46 +0300

    Перенес обращения к БД в другой файл

--- BOT/app/handlers/get_processes/keyboard.py
+++ BOT/app/handlers/get_processes/keyboard.py
@@ -6,60 +6,9 @@ from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from database.TelegramUser.crud import TelegramUserCRUD
-from database.UserInput.crud import UserInputCRUD
 from database.UserInput.model import UserInput
 
 
-#
-# class ProcessInfo(CallbackData, prefix="process_info_kb"):
-#     id: int
-#     queue_guid: str
-#
-#
-# async def stage_selection_kb(suitable_processes: List[UserInput]):
-#     """
-#
-#     :param suitable_processes: Процессы, подходящие под запрос пользователя
-#     :return: Объект inline-клавиатуры
-#     """
-#     keyboard = InlineKeyboardBuilder()
-#     for process in suitable_processes:
-#         callback_data = ProcessInfo(id=process.id, queue_guid=process.queue_guid)
-#
-#         keyboard.add(InlineKeyboardButton(text=f"{process.stage}",
-#                                           callback_data=callback_data.pack()))
-#
-#     return keyboard.adjust(1).as_markup()
-#
-#
-# async def display_mode_selection_keyboard():
-#     # Создаем клавиатуру для предоставления выбора нужного этапа процесса / всех этапов
-#     button_all_stages = types.InlineKeyboardButton(text="Все этапы", callback_data="all_stages")
-#     button_specific_stage = types.InlineKeyboardButton(text="Конкретный этап", callback_data="specific_stage")
-#
-#     # Создаем клавиатуру с кнопками
-#     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
-#         [button_all_stages],  # Первая строка с кнопкой "Все этапы"
-#         [button_specific_stage]  # Вторая строка с кнопкой "Конкретный этап"
-#     ])
-#
-#     return keyboard
-#
-#
-# period_selection_kb = ReplyKeyboardMarkup(
-#     keyboard=[
-#         [
-#             KeyboardButton(text="За текущий час"),
-#             KeyboardButton(text="За текущий день"),
-#             KeyboardButton(text="За текущий год"),
-#         ]
-#
-#     ],
-#     resize_keyboard=False,
-#     one_time_keyboard=True,
-#     input_field_placeholder="Выберите интересующий вас период"
-# )
 class DisplayOptions(enum.Enum):
     ALL_STAGES = "Все этапы"
     SPECIFIC_STAGE = "Конкретный этап"
@@ -130,10 +79,7 @@ class InlineKeyboardsCreator(KeyboardsCreator):
         return self.keyboard.adjust(*sizes).as_markup()
 
 
-async def select_processes_kb(level: int, telegram_id: str, sizes: tuple[int] = (2,)):
-    department = await TelegramUserCRUD.get_department_by_telegram_id(telegram_id)
-    processes = await UserInputCRUD.find_all(department_access=department)
-
+async def select_processes_kb(level: int, processes: List[UserInput], sizes: tuple[int] = (2,)):
     buttons = {str(process.process_name): ProcessInfo(id=process.id, lvl=level + 1).pack()
                for process in processes}
     return "Выберите процесс:", InlineKeyboardsCreator(buttons=buttons).create(sizes=sizes,
@@ -149,10 +95,7 @@ def select_stage_mod_kb(level, sizes: tuple[int] = (2,), display_options: Type[e
                                                                                                     current_level=level)
 
 
-async def stage_selection_kb(level: int, process_id: int, sizes: tuple[int] = (2,)):
-    process = await UserInputCRUD.find_one_or_none(id=process_id)
-    suitable_processes: List[UserInput] = await UserInputCRUD.find_all(process_name=process.process_name)
-
+async def stage_selection_kb(level: int, suitable_processes: List[UserInput], sizes: tuple[int] = (2,)):
     buttons = {
         process.stage: ProcessInfo(lvl=level + 1, stage=process.id).pack()
         for process in suitable_processes
