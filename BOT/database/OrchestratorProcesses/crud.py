0ec2a21508918b1302a4c0b0e41eae738c3d66b2
ilyab <i-baykov@internet.ru>
2024-09-12 16:58:59 +0300

    Создал модель для работы с таблицей OrchestratorProcesses

--- BOT/database/OrchestratorProcesses/crud.py
+++ BOT/database/OrchestratorProcesses/crud.py
@@ -2,7 +2,6 @@ from database.OrchestratorProcesses.model import OrchestratorProcesses
 from database.base_crud import BaseCRUD
 
 
-
 class OrchestratorProcessesCRUD(BaseCRUD):
     model = OrchestratorProcesses
     schema = 'orchestrator'
