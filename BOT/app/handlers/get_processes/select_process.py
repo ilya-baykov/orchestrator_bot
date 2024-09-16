83a094225b3a745e4ffb8313d6fe19b8b3f144a4
ilyab <i-baykov@internet.ru>
2024-09-16 13:22:59 +0300

    Изменил входные данные для select_period_kb ( клавиатура выбора периода фильтрации )

--- BOT/app/handlers/get_processes/select_process.py
+++ BOT/app/handlers/get_processes/select_process.py
@@ -8,7 +8,7 @@ from aiogram.fsm.context import FSMContext
 from app.handlers.get_processes.filter import LevelFilter
 from app.handlers.get_processes.keyboard import *
 from app.handlers.get_processes.message_generation import MessageCreator, TransactionCollector
-from app.handlers.get_processes.state import update_process_stages_if_not_exists
+from app.handlers.get_processes.state import update_process_stages_if_not_exists, update_period_if_not_exists
 from database.TelegramUser.crud import TelegramUserCRUD
 from database.UserInput.crud import UserInputCRUD
 
@@ -22,6 +22,7 @@ orchestrator_process = Router()
 async def handle_process_info(message_or_callback: Union[types.Message, types.CallbackQuery], state: FSMContext):
     """Отображает меню выбора нужного процесса и инициализирует уровень 1"""
     await state.update_data(all_process_stages=None)
+    await state.update_data(period_options=None)
 
     # Получаем процессы, которые относятся к пользователю
     department = await TelegramUserCRUD.get_department_by_telegram_id(telegram_id=str(message_or_callback.from_user.id))
@@ -38,7 +39,10 @@ async def handle_level_2(callback: types.CallbackQuery, state: FSMContext):
     """На этом этапе мы получаем id процесса формируем клавиатуру с выбором периода для фильтрации"""
     callback_data = ProcessInfo.unpack(callback.data)
     if callback_data.id: await state.update_data(id=callback_data.id)  # Фиксируем id выбранного процесса
-    text, reply_markup = select_period_kb(level=callback_data.lvl, sizes=(2,))
+    await update_period_if_not_exists(state)  # Обновляем варианты периода фильтрации один раз для процесса
+    state_data = await state.get_data()  # Получаем данные текущего состояния state
+    text, reply_markup = select_period_kb(level=callback_data.lvl, sizes=(2,),
+                                          ru_periods=state_data.get('period_options').split(';'))
     await callback.message.edit_text(text=text, reply_markup=reply_markup)
 
 
@@ -61,7 +65,7 @@ async def handle_level_4(callback: types.CallbackQuery, state: FSMContext):
     state_data = await state.get_data()  # Получаем всю нужную информацию для формирования запроса
     all_process_stages = state_data.get('all_process_stages', [])  # Информация по всем выбранным этапам
     period = state_data.get('period')  # Информация по периоду фильтрации
-
+    print(state_data)
     if callback_data.mod == DisplayOptions.ALL_STAGES.name:  # Если выбрали 'Показать все этапы'
         stages_info = await TransactionCollector(stages=all_process_stages, filtering_period=period).get_stages_info()
         answer: str = MessageCreator(process_name=f"{'RPA071'}", stages_info=stages_info).answer_text
